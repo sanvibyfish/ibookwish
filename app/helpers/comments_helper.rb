6ee22e77db71971fd03451119947a5d6ce7fe234
sanvibyfish <sanvibyfish@gmail.com>
2012-10-07 15:41:34 +0800

    加入圆梦师HTML代码

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,7 +2,8 @@
 module CommentsHelper
 	def comment_body_tag(comment)
 		comment.body.gsub(/(^|[^a-zA-Z0-9_!#\$%&*@＠])@(.+\s{1})/io) {
-		    %(#{$1}<a href="/#{$2}" class="at_user" title="@#{$2}"><i>@</i>#{$2}</a>)
+		    %(#{$1}<a href="#{user_path(comment.account)}" class="at_user" title="@#{$2}"><i>@</i>#{$2}</a>)
 		}
 	end
+
 end
