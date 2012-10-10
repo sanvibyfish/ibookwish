7892f4bf19692968c88018b0577d76222f114971
sanvibyfish <sanvibyfish@gmail.com>
2012-10-10 16:56:55 +0800

    修改首页评论的数量跟参与数量

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -23,4 +23,19 @@ module PostsHelper
 		link_to image_tag(post.image.url(style)), post_path(post),:class => options[:class]
 	end
 
+	def join_count_tag(post)
+		if post.wish_users?
+			link_to post.wish_users.count, post_path(post)
+		else
+			link_to "0", post_path(post)
+		end
+	end
+
+	def comment_count_tag(post)
+		if post.comments?
+			link_to post.comments.count, post_path(post)
+		else
+			link_to "0", post_path(post)
+		end
+	end
 end
