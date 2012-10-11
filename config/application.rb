0b4837013f2ddcbfefeba6e4eda20ae772084089
sanvibyfish <sanvibyfish@gmail.com>
2012-10-11 12:56:54 +0800

    继续添加production参数

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require "sprockets/railtie" # Uncomment this line for Rails 3.1+
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  Bundler.require(*Rails.groups(:assets => %w(production development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
