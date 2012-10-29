db9640f2e56bcdf4c7cb8321e887f343d7387a2e
sanvibyfish <sanvibyfish@gmail.com>
2012-10-29 15:04:17 +0800

    在helper里面初始化location

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,19 +1,3 @@
 class InvitationsController < Devise::InvitationsController
-  before_filter :location
-  def location 
-    if session[:location].blank?
-      # FIXME 目前是虚拟IP
-      if request.location.blank?
-        session[:location] =  Location.find_by(pin_yin: "shenzhen")
-      elsif request.location.city.downcase.blank?
-        session[:location] =  Location.find_by(pin_yin: "shenzhen")
-      else
-        session[:location] = Location.where(pin_yin: request.location.city.downcase).first
-        if session[:location].blank?
-           session[:location] =  Location.find_by(pin_yin: "shenzhen")
-        end
 
-      end 
-    end
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,7 +18,26 @@ module ApplicationHelper
 		#{link_to("深圳", "/bbs/topics/node_508bf749b37295dc4e000004")}|
 		#{link_to("广州", "/bbs/topics/node_508bf749b37295dc4e000005")}|
 		#{link_to("杭州", "/bbs/topics/node_508bf749b37295dc4e000006")}|
-		#{link_to("广州", "/bbs/topics/node_508bf749b37295dc4e000007")}")
+		#{link_to("宁波", "/bbs/topics/node_508bf749b37295dc4e000007")}")
 	end
 
+
+	def init_location
+    if session[:location].blank?
+      # FIXME 目前是虚拟IP
+      if request.location.blank?
+        session[:location] =  Location.find_by(pin_yin: "shenzhen")
+      elsif request.location.city.downcase.blank?
+        session[:location] =  Location.find_by(pin_yin: "shenzhen")
+      else
+        session[:location] = Location.where(pin_yin: request.location.city.downcase).first
+        if session[:location].blank?
+           session[:location] =  Location.find_by(pin_yin: "shenzhen")
+        end
+
+      end 
+    end
+	end
+
+  
 end
