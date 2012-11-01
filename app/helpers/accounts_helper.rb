a8fffd1d6795a324177b475ce8d8a760c2ecb045
sanvibyfish <sanvibyfish@gmail.com>
2012-11-01 16:12:35 +0800

    修复用户列表签名过长导致错位

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -4,7 +4,7 @@ module AccountsHelper
     if user.tagline.blank?
       "暂无签名"
     else
-     user.tagline 
+    	truncate(strip_tags(user.tagline), :length => 30, :omission => '...')
     end
   end
 
