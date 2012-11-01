d98e41d132928e4203937fdf595d41dafbc928e6
sanvibyfish <sanvibyfish@gmail.com>
2012-11-01 15:37:36 +0800

    评论@相同用户bug

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -21,7 +21,6 @@ class Comment
     post = comment.post
     return if post.blank?
     # 给发帖人发回帖通知
-
     if comment.user_id != post.user_id
      notif =  Notification.create :from_user => comment.user_id, :to_user => comment.post.user_id, :body => comment.body, 
         :notif_type => Notification::TYPE[:reply], :post => comment.post
@@ -29,15 +28,18 @@ class Comment
         comment.post.user.notification_ids.push(notif.id)
     end
 
-    
+    send_ids = []
     #给@的人发通知
     comment.body.scan(%r{@[a-zA-Z0-9_\u4e00-\u9fa5]{1,20}}).each { |name|
         user = User.find_by(:name => name.delete("@"))
         next if user.blank?
+        unless send_ids.include?(user.id)
      notif_at =   Notification.create :from_user => comment.user_id, :to_user => user.id, :body => comment.body, 
         :notif_type => Notification::TYPE[:at], :post => comment.post
         comment.user.send_ids.push(notif_at.id)
         user.notification_ids.push(notif_at.id)
+        send_ids << user.id
+       end
     }
 
 
