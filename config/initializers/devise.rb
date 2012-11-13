a4ddb94ee4f2a48096d454067a03cf5d9c397493
sanvibyfish <sanvibyfish@gmail.com>
2012-11-13 15:17:55 +0800

    本次更新 -改成info@ibookwish.com

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,7 +5,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "ibookwish<no-reply@ibookwish.com>"
+  config.mailer_sender = "ibookwish<info@ibookwish.com>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
