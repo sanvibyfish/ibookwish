a5fcd76c71de2e1697fc1b3a1aae55fe7bb23980
sanvibyfish <sanvibyfish@gmail.com>
2012-11-26 16:39:08 +0800

    --增加首页显示评论 --更改图片URL

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,5 +3,5 @@ CarrierWave.configure do |config|
   config.upyun_username = "sanvi1020"
   config.upyun_password = 'air20dream'
   config.upyun_bucket = "image-sanvibyfish"
-  config.upyun_bucket_domain = "image-sanvibyfish.b0.upaiyun.com"
+  config.upyun_bucket_domain = "image.ibookwish.com"
 end
