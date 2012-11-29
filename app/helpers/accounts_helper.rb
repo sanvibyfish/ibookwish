89ccb4a36721c0f7b255c67ee8fe3d62517ecc29
sanvibyfish <sanvibyfish@gmail.com>
2012-11-29 15:26:20 +0800

    --修改书友样式 --修改图书部分详情

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -8,6 +8,21 @@ module AccountsHelper
     end
   end
 
+  def user_avatar_custom_tag(user, options = {})
+		options[:style] ||= :small
+		style = case options[:style].to_s
+		when "small" then "30x30"
+		when "normal" then "100x100"
+		when "240x240" then "240x240"
+		when "tiny" then "20x20"
+		when "48x48" then "48x48"
+		else options[:style].to_s
+		end
+		
+		link_to image_tag(user.avatar.url(style), :rel => "twipsy", "data-placement" => "bottom" ,
+			"data-original-title" => "#{options[:title]}",
+		 :class => "img-rounded #{options[:image_css]}"), user_path(user),:class => options[:class]
+	end
 
 	def user_avatar_tag(user, options = {})
 		options[:style] ||= :small
@@ -23,5 +38,15 @@ module AccountsHelper
 		link_to image_tag(user.avatar.url(style), :rel => "twipsy", "data-placement" => "bottom" ,
 			"data-original-title" => "#{user.name}<br/>#{user_tagline_tag(user)}<br/>#{user.location.name unless user.location.blank?}",
 		 :class => "img-rounded #{options[:image_css]}"), user_path(user),:class => options[:class]
+	end
+
+	def image_clip(url, options= {})
+		options[:style] ||= :small
+		style = case options[:style].to_s
+		when "240x240" then "240x240"
+		when "360x360" then "360x360"
+		else options[:style].to_s
+		end
+		image_tag "#{url}!#{style}"
 	end
 end
